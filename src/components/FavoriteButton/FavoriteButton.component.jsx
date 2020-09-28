35d50f802f5156a8c6362d0c1eeb95f1c73958f0
Mark Rivera <mark.rivera@wizeline.com>
2020-09-28 12:42:50 -0500

    Video Provider and Add Favorites Button

---
+++ src/components/FavoriteButton/FavoriteButton.component.jsx
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react';
+import Button from 'react-bootstrap/Button';
+import UserContext from '../../providers/UserProvider/User.context';
+import useVideoStatus from '../../utils/hooks/useVideoStatus';
+
+function FavoriteButton({ video }) {
+  const { addFavorite, removeFavorite } = useContext(UserContext);
+  const videoStatus = useVideoStatus(video);
+  function addNewFavorite() {
+    addFavorite(video);
+  }
+
+  function removeFromFavorites() {
+    removeFavorite(video);
+  }
+
+  const buttonAdd = (
+    <Button onClick={addNewFavorite} variant="outline-info">
+      {' '}
+      Add to favorite
+    </Button>
+  );
+  const buttonRemove = (
+    <Button onClick={removeFromFavorites} variant="outline-dark">
+      {' '}
+      Remove from Favorites
+    </Button>
+  );
+
+  return videoStatus ? buttonRemove : buttonAdd;
+}
+
+export default FavoriteButton;
