677027e40bad21314e137ad7d96c5d6867977251
Mark Rivera <mark.rivera@wizeline.com>
2020-09-28 12:40:42 -0500

    VideoList component on Home

--- src/components/Layout/Layout.component.jsx
+++ src/components/Layout/Layout.component.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 import './Layout.styles.css';
 
 function Layout({ children }) {
-  return <main className="container">{children}</main>;
+  return (
+    <div className="page-container">
+      <main className="container">{children}</main>
+    </div>
+  );
 }
 
 export default Layout;
---
+++ src/components/VideoList/VideoList.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import VideoItem from '../VideoItem';
+
+const VideoList = ({ videos }) => {
+  const renderedVideos = videos.map((video) => {
+    return <VideoItem key={video.id.videoId} video={video} />;
+  });
+
+  return <ul className="list-unstyled">{renderedVideos}</ul>;
+};
+export default VideoList;
