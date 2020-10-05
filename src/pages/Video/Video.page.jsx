1a31951ea61bd4a4bd0af08a94845cfbf62debc6
Mark Rivera <mark.rivera@wizeline.com>
2020-10-05 02:36:14 -0500

    add method to access to video detail by its ID

--- src/pages/Video/Video.page.jsx
+++ src/pages/Video/Video.page.jsx
@@ -1,17 +1,56 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Video.styles.css';
 import Container from 'react-bootstrap/Container';
+import { useParams } from 'react-router-dom';
 import VideoList from '../../components/VideoList';
 import VideoDetail from '../../components/VideoDetail/VideoDetail.component';
-import VideoContext from '../../providers/VideoProvider/Video.context';
+import youtube from '../../api/youtube';
+
+async function getVideoDetails(videoId) {
+  const videoDetail = await youtube.get('/videos', {
+    params: {
+      part: 'snippet',
+      id: videoId,
+    },
+  });
+
+  return videoDetail;
+}
+
+async function getReccomendations(videoId) {
+  const videoDetail = await youtube.get('/search', {
+    params: {
+      part: 'snippet',
+      relatedToVideoId: videoId,
+      type: 'video',
+    },
+  });
+  return videoDetail;
+}
 
 function VideoPage() {
-  const { videoState } = useContext(VideoContext);
+  const { videoId } = useParams();
+  const [videoDetail, setvideoDetail] = useState({});
+  const [relatedVideos, setrelatedVideos] = useState({});
+
+  useEffect(() => {
+    console.log('trying to change video detail');
+    if (!videoDetail.id || videoId !== videoDetail.id) {
+      getVideoDetails(videoId).then((result) => {
+        setvideoDetail(result.data.items[0]);
+      });
+    } else {
+      console.log(videoDetail);
+      getReccomendations(videoDetail.id).then((reccomendations) => {
+        setrelatedVideos(reccomendations.data.items);
+      });
+    }
+  }, [videoId, videoDetail]);
   return (
     <Container className="wrapper">
-      <VideoDetail />
+      <VideoDetail videoDetail={videoDetail} />
       <div className="list-container">
-        <VideoList videos={videoState.videoList} />
+        {relatedVideos.length && <VideoList videos={relatedVideos} />}
       </div>
     </Container>
   );
